2786d5cd573351c9d91fb9ee482d3f9fb4d1dde2
asalazar <a.salazar096@gmail.com>
2023-03-19 20:02:30 -0300

    dieciséis commit

--- src/routes/middelware/authenticate.js
+++ src/routes/middelware/authenticate.js
@@ -1,15 +1,23 @@
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
 const authenticateToken = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split('Bearer ')[1] || '';
-        req.user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        next();
-    } catch (error) {
-        res.status(401).json({
-            "message": "token invalido"
-        })
-    }
+  try {
+    const token = req.headers.authorization.split('Bearer ')[1] || '';
+    req.user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    next();
+  } catch (error) {
+    res.status(401).json({
+      "message": "token invalido"
+    })
+  }
 }
+
+const corsOptions = {
+  origin: 'https://optimum-web.vercel.app',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 module.exports = authenticateToken;
